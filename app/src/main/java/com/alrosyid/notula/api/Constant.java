e7673a32e0344cd9ad51dc4e894460cea98d711d
Harun Al Rosyid/170101010 <38546311+harun248@users.noreply.github.com>
2021-03-13 18:41:39 +0000

    redesign login layout

--- app/src/main/java/com/alrosyid/notula/api/Constant.java
+++ app/src/main/java/com/alrosyid/notula/api/Constant.java
@@ -1,7 +1,7 @@
 package com.alrosyid.notula.api;
 
 public class Constant {
-    public static final String URL = "http://192.168.100.4:8000/";
+    public static final String URL = "http://192.168.43.38:8000/";
     public static final String HOME = URL+"api";
     public static final String LOGIN = HOME+"/login";
     public static final String LOGOUT = HOME+"/logout";
